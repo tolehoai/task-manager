bb1e7478ff74ea23e31e20311f557808397ffc1c
tolehoai <tolehoai@gmail.com>
2022-01-11 08:34:27 +0700

    add task list

---
+++ src/Components/TaskList/style.js
@@ -0,0 +1,14 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  taskList: {
+    margin: "30px 10px",
+  },
+  name: {
+    fontSize: "30px",
+    fontWeight: "800",
+    color: "#1a2e57",
+    marginBottom: "15px",
+  },
+});
+export { useStyles };
